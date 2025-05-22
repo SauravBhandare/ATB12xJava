9c1168ad036d82deae08e29bba43781fc7a2ce04
SauravBhandare <sauravkb07@gmail.com>
2025-05-23 00:19:25 +0530

    23 May 2025

---
+++ src/ex_07_Increment_Decrement_Operator/Lab_069_Inc_Dec_Operators.java
@@ -0,0 +1,17 @@
+package ex_07_Increment_Decrement_Operator;
+
+public class Lab_069_Inc_Dec_Operators {
+
+    public static void main(String[] args) {
+
+        String age_string=  args[0];
+        String str2 = args[1];
+
+        int i1= Integer.parseInt(age_string);
+        int i2= Integer.parseInt(str2);
+        System.out.println(age_string);
+        System.out.println(str2);
+
+
+    }
+}
