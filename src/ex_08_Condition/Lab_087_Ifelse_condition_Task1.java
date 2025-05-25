beeaa7ea991ee830e0867994c886c3dd64a0ee20
SauravBhandare <sauravkb07@gmail.com>
2025-05-25 11:28:08 +0530

    22 May 2025

--- src/ex_08_Condition/Lab_087_Ifelse_condition_Task1.java
+++ src/ex_08_Condition/Lab_087_Ifelse_condition_Task1.java
@@ -1,25 +1,36 @@
 package ex_08_Condition;
 
+
 import java.util.Scanner;
 
 public class Lab_087_Ifelse_condition_Task1 {
 
+    //Write a program that classifies a triangle based on its side lengths.
+    // Given three input values representing the lengths of the sides, determine if the triangle is equilateral (all sides are equal),
+    // isosceles (exactly two sides are equal), or scalene (no sides are equal).
+    // Use an if-else statement to classify the triangle.
+
     public static void main(String[] args) {
 
-        System.out.println("Enter the lengths of triangle");
-      Scanner scanner = new Scanner(System.in);
-      int s1 = scanner.nextInt() ;
-      int s2 = scanner.nextInt();
-      int s3 = scanner.nextInt();
+        System.out.println("Enter the lengths of side");
 
-      if (s1==s2)
-      {
-          System.out.println("Isosceles");
-      }
-      else if (s1s2)
-      {
-          System.out.println();
-      }
+        Scanner scanner =new Scanner(System.in);
+        int l1 = scanner.nextInt();
+        int l2 = scanner.nextInt();
+        int l3 = scanner.nextInt();
+
+        if (l1 == l2 && l2 == l3)
+        {
+            System.out.println("Equilateral Triangle");
+        }
+        else if (l1!=l2 && l2!=l3)
+        {
+            System.out.println("Scalene Triangle");
+        }
+        else
+        {
+            System.out.println("Isosceles Triangle");
+        }
 
 
     }
